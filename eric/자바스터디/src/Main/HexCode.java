a37a874427515bcf0b1cb622298e98ffa9f07e69
Err0R <kmk5158@kakao.com>
2020-12-30 22:54:36 +0900

    2020.12.30 solved.ac
    브론즈5 1~9번 Java 코드

---
+++ eric/자바스터디/src/Main/HexCode.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class HexCode{
+    public static void main(String[] args) throws IOException{
+
+        Scanner scan = new Scanner(System.in);
+
+        String n = scan.next();
+
+        System.out.println(Integer.parseInt(n, 16));
+    }
+}
